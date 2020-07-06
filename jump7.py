5bb84154b46912b4d22945277fd99bf6c432acf0
wx0020 <4152443@qq.com>
2020-07-06 23:11:42 +0800

    '123'

---
+++ jump7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# for letter in 'Python':  # 第一个实例
+#     print('当前字母 :', letter)
+#
+# fruits = ['banana', 'apple', 'mango']
+# for fruit in fruits:  # 第二个实例
+#     print('当前水果 :', fruit)
+
+
+i = 0;
+for i in range(1,101):
+    if i % 7 != 0:
+        print('i:', i)
+
+
+print("Hello, World!")
